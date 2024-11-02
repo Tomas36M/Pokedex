03e0cffca878bc4048e8b37a9030a8a7aac63a0f
Tomas36M <tomasmunevar36@gmail.com>
2024-11-01 20:17:39 -0500

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,8 @@ function Navbar({ onSearchChange }) {
     return (
         <nav className="bg-gradient-to-r from-[#641a19] to-[#5e5e5c] shadow-lg">
             <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-                <div className="flex items-center">
-                    <Link to='/'><span className="text-3xl font-logo text-white">Pokedex</span></Link>
-                </div>
-                <div className="flex lg:hidden">
+                <Link to='/' className="text-3xl font-logo text-white">Pokedex</Link>
+                <div className="lg:hidden">
                     <button
                         className="text-white"
                         onClick={() => setIsOpen(!isOpen)}
@@ -20,11 +18,11 @@ function Navbar({ onSearchChange }) {
                         </svg>
                     </button>
                 </div>
-                <div className={`flex-col lg:flex-row lg:flex items-center ${isOpen ? 'flex' : 'hidden'} space-y-2 lg:space-y-0`}>
+                <div className={`flex-col lg:flex-row lg:flex items-center ${isOpen ? 'flex' : 'hidden'} lg:space-y-0 space-y-2 lg:ml-4`}>
                     <input
                         type="text"
                         placeholder="Search Pokemon"
-                        className="mt-2 lg:mt-0 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 lg:w-auto w-full"
+                        className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full lg:w-64 lg:max-w-full"
                         onChange={(e) => onSearchChange(e.target.value)}
                     />
                 </div>
